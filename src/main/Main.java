e69c6cd4ea3e0990dc5998ef42829393575898e8
Daniel Gallagher <danielgallagher500@gmail.com>
2020-04-21 14:57:05 +0100

    Basic HuffmanTree Setup

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,16 +2,40 @@ package src.main;
 
 import src.huffman.HuffmanTree;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        String test = "loiosoeelllllleur";
+    public static void main(String[] args) throws UnsupportedEncodingException {
 
-        HuffmanTree tree = new HuffmanTree(test);
+        //--BEFORE COMPRESSION--
 
-        System.out.println(tree.getCompressedOutput());
+        String INPUT = "Quarantine";
+
+        System.out.println("Input String: " + INPUT + "\n");
+
+        System.out.println("Before Compression:");
+        for(byte b : INPUT.getBytes())
+        {
+            System.out.print(Integer.toBinaryString(b));
+        }
+        System.out.println();
+
+        //--COMPRESSION--
+
+        //Pass A String To The Tree
+        HuffmanTree tree = new HuffmanTree(INPUT);
+
+        //Retrieve The Bits After Compression
+        String compressedBits = tree.getCompressedBits();
+        System.out.println("\nAfter Compression:\n" + compressedBits + "\n");
+
+        //--DECOMPRESSION--
+
+        String decompression = tree.decompressBits(compressedBits);
+
+        System.out.println("Pass Bits Through Decompression:");
+        System.out.println(decompression);
     }
 }
