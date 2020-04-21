b10b6c9b4f940eaced4739b840b81bddea8c34e6
Daniel Gallagher <danielgallagher500@gmail.com>
2020-04-21 14:57:20 +0100

    Decompression Implemented

--- src/huffman/HuffmanTree.java
+++ src/huffman/HuffmanTree.java
@@ -9,14 +9,17 @@ import java.util.Set;
 
 public class HuffmanTree
 {
-
+	//Table Of How Frequent Each Letter is
     private FrequencyTable freqTable;
 
+    //Encodings For Char => Binary Value
     private final HashMap<Character, String> encodings;
 
+    //Our Tree's Root Node
     private Node ROOT;
 
-    private String compressedOutput = "";
+    //Our Compressed Bit-string
+    private String compressedBits = "";
 
     public HuffmanTree(String input)
     {
@@ -60,45 +63,111 @@ public class HuffmanTree
 		for(char c : text)
 		{
 			String encodedChar = encodings.get(c);
-			compressedOutput += encodedChar;
+			compressedBits += encodedChar;
 		}
     }
 
-    public String getCompressedOutput()
+    public String decompressBits(String input)
 	{
-		return this.compressedOutput;
+		char nxt;
+		boolean canMoveLeft, canMoveRight, endOfInput;
+
+		Node   n = ROOT;
+		int    size = input.length();
+		String translation = "";
+
+		for(int i = 0; i < size; i++)
+		{
+			//Get Next Character
+			nxt = input.charAt(i);
+
+			//Error Handling: Input Must Be Binary
+			if(nxt != '0' && nxt != '1')
+				throw new IllegalArgumentException("Input Must Be A Bit-String");
+
+			/*
+			Boolean Variables:
+			- Can we move left in the tree to a another node.
+			- Can we move right in the tree to another node.
+			- Are we on the final character in our input
+			 */
+			canMoveLeft  = (nxt == '0' && n.hasLeft());
+			canMoveRight = (nxt == '1' && n.hasRight());
+			endOfInput   = (i == (size - 1));
+
+			if(canMoveLeft)
+			{
+				//N Moves Left
+				n = n.getLeft();
+
+				//Check If We're On Final Character
+				if(endOfInput)
+					translation += n.getChar();
+
+			}
+			else if (canMoveRight)
+			{
+				//N Moves Right
+				n = n.getRight();
+
+				//Check If We're On Final Character
+				if(endOfInput)
+					translation += n.getChar();
+			}
+			else
+			{
+				/*
+				We can't move left or right in the
+				trie. This means that we have arrived
+				at our decompressed letter.
+
+				We add this to the translation,
+				then reset our node and take our for
+				loop back to begin from the root node
+				for our next pattern.
+				 */
+
+				//Add Decompressed Character
+				if(n.hasChar())
+					translation += n.getChar();
+
+				//Reset Node & Begin Next Pattern
+				n = ROOT;
+				i--;
+			}
+		}
+
+		return translation;
 	}
 
-    public HashMap<Character, String> getEncodings()
-    {
-    	return this.encodings;
-    }
-
+	/* Create a new node that contains
+	   Node i and Node j as it's left
+	   and right variable. */
     private Node connectNodes(ArrayList<Node> nodes, int i, int j)
     {
+    	//Invalid Node Array
     	if(nodes.size() < 2)
     		return null;
 
+    	//Get The Nodes
     	Node n1 = nodes.get(i);
 		Node n2 = nodes.get(j);
 
+		//Combined Frequency of Two Nodes
 		int totalFreq = n1.getFrequency() + n2.getFrequency();
 
+		//Create New Node
 		Node newNode = new Node(totalFreq);
 
+		//Connect The Nodes
 		newNode.setLeft(n1);
 		newNode.setRight(n2);
     	
     	return newNode;
     }
 
-    public FrequencyTable getTable()
-    {
-		return this.freqTable;
-    }
-
-    public Node getRoot()
-    {
-    	return this.ROOT;
-    }
+	//Accessor Methods
+	public HashMap<Character, String> getEncodings() { return this.encodings; }
+	public String getCompressedBits()				 { return this.compressedBits; }
+	public FrequencyTable getTable()				 { return this.freqTable; }
 }
